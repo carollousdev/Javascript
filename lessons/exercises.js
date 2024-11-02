ded27868249c5ca7f077bd0dcd7f6030a92a3d74
Carollous Dachi <166882001+carollousdev@users.noreply.github.com>
2024-11-02 21:59:46 +0700

    Exercise chapter one

---
+++ lessons/exercises.js
@@ -0,0 +1,15 @@
+let person = {
+    name: 'John Witch',
+    age: 38,
+    gender: 'Male',
+    isMarried: true,
+    location: {
+        long: 31.000000,
+        lat: (-100.000000)
+    },
+    friends: ['Reyes', 'Rames', 'Ricky', 'Rocky']
+}
+
+console.log(person)
+console.log(person.location['lat'])
+console.log(person.friends['0'])
