923e7d4ac678d1b1beeeff3c3488cf043f6be2f3
Carollous Dachi <166882001+carollousdev@users.noreply.github.com>
2024-11-02 21:46:44 +0700

    Template Literals

---
+++ lessons/templateliterals.js
@@ -0,0 +1,8 @@
+let names = 'John Witch'
+console.log(`Your name is ${names}`) // Output: Your name is John Witch
+console.log(`${30 + 24}`) // Output: 54
+let message = `My name is \n ${names}`
+console.log(message)
+// Output:
+// My name is
+//  John Witch
